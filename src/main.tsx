154db0e4d07394c13336d7f2ccba53a5d8638b94
Dat Dang <black.bc01@gmail.com>
2025-03-14 02:34:43 +0700

    connect wallet

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,34 @@
 import './main.scss';
+import '@mysten/dapp-kit/dist/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router';
 
+import suiSdkTheme from '@components/suiSdkTheme';
 import App from './App';
+
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl('localnet') },
+  devnet: { url: getFullnodeUrl('devnet') },
+  testnet: { url: getFullnodeUrl('testnet') },
+  mainnet: { url: getFullnodeUrl('mainnet') },
+});
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider theme={suiSdkTheme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
--- src/screens/swap/index.tsx
+++ src/screens/swap/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { ConnectButton, useCurrentAccount, useSuiClientQuery } from '@mysten/dapp-kit';
 
 const ScreenSwap: React.FC = () => {
+  const account = useCurrentAccount();
+  console.log(account);
+  const { data: balanceSui } = useSuiClientQuery('getBalance', {
+    owner: account?.address || '',
+    coinType: '0x2::sui::SUI',
+  });
+  console.log(balanceSui);
+  const { data: balanceUsdc } = useSuiClientQuery('getBalance', {
+    owner: account?.address || '',
+    coinType: '0x168da5bf1f48dafc111b0a488fa454aca95e0b5e::usdc::USDC',
+  });
+  console.log(balanceUsdc);
+
   return (
     <>
-      <div className="h-full flex justify-center"></div>
+      <div className="h-full">
+        {!account && (
+          <>
+            <div className="flex flex-row justify-center mt-5">
+              <ConnectButton />
+            </div>
+          </>
+        )}
+        {!!account && <></>}
+      </div>
     </>
   );
 };

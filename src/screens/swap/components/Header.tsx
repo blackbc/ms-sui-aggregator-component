0d03934b6a8d129181b06b1ce52f7e90d275883c
Dat Dang <black.bc01@gmail.com>
2025-03-14 17:47:33 +0700

    header component

--- src/screens/swap/components/Header.tsx
+++ src/screens/swap/components/Header.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import imgSlippage from '@assets/images/slippage.png';
+
 const ScreenSwapHeader: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-row justify-between items-center">
+        <p className="text-white text-lg font-medium">Swap</p>
+        <div className="flex flex-row justify-center items-center">
+          <img src={imgSlippage} className="w-5 h-5" alt="slippage" />
+          <p className="text-white text-md font-medium ml-1">0.5%</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ScreenSwapHeader;
